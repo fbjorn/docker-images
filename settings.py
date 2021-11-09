f083c47bf22d7c1c68f1614f0a244314e1e1d1d8
fbjorn <fbjorn@users.noreply.github.com>
2021-11-09 17:33:50 +0300

    Add alpine image

--- settings.py
+++ settings.py
@@ -4,10 +4,10 @@ from pydantic import BaseSettings
 # Any of these can be overridden via environment variables with matching names
 class Settings(BaseSettings):
     # User or organization name, used for "user/image" -name generation
-    DOCKER_USER = "lietu"
+    DOCKER_USER = "fbjorn"
 
     # List of images that should be built beforehand
-    PRIORITY_BUILDS = ["python-base/ubuntu20.04-python3.9"]
+    PRIORITY_BUILDS = []
 
 
 conf = Settings()
