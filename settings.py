908528f613060b304e24c883c52aaa44cf5d68ee
fbjorn <fbjorn@users.noreply.github.com>
2022-12-11 20:41:16 +0400

    Merge latest changes

--- settings.py
+++ settings.py
@@ -4,18 +4,15 @@ from pydantic import BaseSettings
 # Any of these can be overridden via environment variables with matching names
 class Settings(BaseSettings):
     # User or organization name, used for "user/image" -name generation
-    DOCKER_USER = "lietu"
+    DOCKER_USER = "fbjorn"
 
     # List of images that should be built beforehand
     PRIORITY_BUILDS = [
         [
-            "ubuntu-base/20.04",
             "ubuntu-base/22.04",
         ],
         [
-            "python-base/ubuntu20.04-python3.9",
             "python-base/ubuntu22.04-python3.10",
-            "python-base/ubuntu22.04-python3.11",
             "nginx-base/alpine-nginx",
         ],
     ]
